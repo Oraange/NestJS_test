84ee69d572e1e41f81d13806faac94a38f07df0b
Chi heon Song <hrpp1353@gmail.com>
2022-01-06 17:05:47 +0900

    feat: Create CRUD Board API
    - CRUD 구현
      - 게시글 생성
      - 게시글 전체 조회
      - 게시글 상세 조회
      - 게시글 공개 상태 수정
      - 게시글 삭제

--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@nestjs/common';
-import { Board } from './board.model';
+import { Board, BoardStatus } from './board.model';
+import { v1 as uuid } from 'uuid';
+import { CreateBoardDto } from './dto/create-board.dto';
 
 @Injectable()
 export class BoardsService {
@@ -8,4 +10,32 @@ export class BoardsService {
     getAllBoards(): Board[] {
         return this.boards;
     }
+
+    createBoard(createBoardDto: CreateBoardDto) {
+        const { title, description } = createBoardDto;
+        const board: Board = {
+            id: uuid(),
+            title, //변수명과 타입명이 같을 시 타입을 생략 가능
+            description,
+            status: BoardStatus.PUBLIC
+        }
+        
+        this.boards.push(board)
+        return board;
+    }
+
+    getBoardById(id: string): Board {
+        return this.boards.find((board) => board.id === id);
+    }
+
+    deleteBoard(id: string): void {
+        this.boards = this.boards.filter((board) => board.id !== id);
+    }
+
+    updateBoardStatus(id: string, status: BoardStatus): Board {
+        const board = this.getBoardById(id);
+        board.status = status;
+        return board;
+    }
+
 }
